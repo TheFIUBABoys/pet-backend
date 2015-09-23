94de2498ca784ab99805c44772e6fc84bdc7ff48
Gonzalo Beviglia <gonzalo.beviglia@restorando.com>
2015-09-23 18:43:02 -0300

    Show images on show/

--- app/helpers/pets_helper.rb
+++ app/helpers/pets_helper.rb
@@ -1,16 +1,16 @@
 module PetsHelper
 
-  def pet_type_options(selected)
+  def pet_type_options(selected = nil)
     options = Pet::TYPES.map { |type| [I18n.t("pets.#{type.downcase}"), type] }
     options_for_select options, selected
   end
 
-  def pet_gender_options(selected)
+  def pet_gender_options(selected = nil)
     options = Pet::GENDERS.map { |gender| [I18n.t("pets.#{gender}"), gender] }
     options_for_select options, selected
   end
 
-  def boolean_options(selected)
+  def boolean_options(selected = nil)
     options_for_select [[I18n.t("shared.yes_word"), true], [I18n.t("shared.no_word"), false]], selected
   end
 
