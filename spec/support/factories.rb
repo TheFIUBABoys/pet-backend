be2f49fa014263a6bf003eaed94c8034f4c912d1
Gonzalo Beviglia <gonzalo.beviglia@restorando.com>
2015-09-23 12:06:58 -0300

    User sign up spec for fb and email.

--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -1,12 +1,19 @@
 FactoryGirl.define do
 
   factory :user, class: User do
-    email { Forgery(:internet).email_address }
-    password { Forgery(:basic).text(at_least: 8) }
+    trait :facebook do
+      facebook_id { Forgery(:basic).text(at_least: 8) }
+      facebook_token { Forgery(:basic).text(at_least: 8) }
+    end
+
+    trait :email do
+      email { Forgery(:internet).email_address }
+      password { Forgery(:basic).text(at_least: 8) }
+    end
   end
 
   factory :pet, class: Pet do
-    user(factory: :user)
+    association :user, :email
     name { Forgery(:name).first_name }
 
     trait :dog do
