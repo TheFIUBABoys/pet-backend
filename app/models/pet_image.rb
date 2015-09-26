edf02f5acc1bdb1137097e20ae6e9995d3243e97
Gonzalo Beviglia <gonzalo.beviglia@restorando.com>
2015-09-26 20:44:33 -0300

    Possible fix.

--- app/models/pet_image.rb
+++ app/models/pet_image.rb
@@ -2,4 +2,11 @@ class PetImage < ActiveRecord::Base
   belongs_to :pet
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  
+  before_validation(on: :create) do |file|
+    if file.image_content_type == "application/octet-stream"
+      mime_type = MIME::Types.type_for(file.image_file_name)
+      file.image_content_type = mime_type.first.content_type if mime_type.first
+    end
+  end
 end
