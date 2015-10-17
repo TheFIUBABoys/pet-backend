ca92cded02608bfcb387bd021c0c843ea4e7cc02
Gonzalo Beviglia <gonzalo.beviglia@restorando.com>
2015-10-17 15:57:35 -0300

    Update seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,14 +2,15 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
 password = Forgery(:basic).text(at_least: 8)
-user = User.create(email: Forgery(:internet).email_address, password: password, password_confirmation: password)
+user_1 = User.create(email: Forgery(:internet).email_address, password: password, password_confirmation: password)
+user_2 = User.create(email: Forgery(:internet).email_address, password: password, password_confirmation: password)
 
 20.times do
   type   = Pet::TYPES.sample
   gender = Pet::GENDERS.sample
 
-  Pet.create(
-    user: user,
+  pet = Pet.create(
+    user: user_1,
     type: type,
     gender: gender,
     name: Forgery(:name).first_name,
@@ -19,4 +20,7 @@ user = User.create(email: Forgery(:internet).email_address, password: password, 
     location: "#{Forgery(:geo).latitude},#{Forgery(:geo).longitude}",
     vaccinated: Forgery(:basic).boolean,
     needs_transit_home: Forgery(:basic).boolean)
+
+  question = PetQuestion.create(pet: pet, user: user_2, body: Forgery(:lorem_ipsum).text)
+  answer   = PetQuestionAnswer.create(pet_question: question, body: Forgery(:lorem_ipsum).text)
 end
