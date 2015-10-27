5a2a32cdea85afcec59a0932cb72f3c67312c92e
Gonzalo Beviglia <gonzalo.beviglia@restorando.com>
2015-10-27 00:24:23 -0300

    Fix spec.

--- app/services/pet_create_service.rb
+++ app/services/pet_create_service.rb
@@ -7,10 +7,8 @@ class PetCreateService < BaseService
   def call(pet_attributes)
     pet_videos = pet_attributes.delete(:videos)
 
-    pet = @user.pets.create(pet_attributes)
+    pet = @user.pets.create(pet_attributes.merge(published: true))
     pet = create_videos(pet, pet_videos) if pet_videos.present?
-
-    pet.publish!
 
     ModelResponse.new(pet)
   end
